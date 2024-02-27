64cdca08b7995a06fe7d896f61bb745c1fd33484
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-02-27 12:37:07 +0100

    Add a duplicate method

--- src/main/java/com/tolache/mavensample/App.java
+++ src/main/java/com/tolache/mavensample/App.java
@@ -8,12 +8,30 @@ public class App
     public static void main( String[] args )
     {
         testTmpDir();
+        duplicateMethod();
         String version = App.class.getPackage().getImplementationVersion();
         System.out.println("Version is: " + version);
         System.out.println(HeapChecker.showHeapInfo());
     }
 
     private static void testTmpDir() {
+        String defaultBaseDir = System.getProperty("java.io.tmpdir");
+        System.out.println("tmpdir is: " + defaultBaseDir);
+        try {
+            File myObj = new File(defaultBaseDir + "new_file.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + defaultBaseDir + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    // for testing Duplicate Finder
+    private static void duplicateMethod() {
         String defaultBaseDir = System.getProperty("java.io.tmpdir");
         System.out.println("tmpdir is: " + defaultBaseDir);
         try {
